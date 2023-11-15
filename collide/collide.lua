fc4976622d59945218a62f18527a4440a14a8316
William Sasaki <65739117+wsasaki01@users.noreply.github.com>
2023-11-15 00:30:19 +0000

    Reuce token count on collide

--- collide/collide.lua
+++ collide/collide.lua
@@ -1,4 +1,4 @@
 function collide(x1,y1,w1,h1,x2,y2,w2,h2)
 -- check collision between two rectangles
- return abs((x2+(w2/2))-(x1+(w1/2)))<=(w1/2)+(w2/2) and abs((y2+(h2/2))-(y1+(h1/2)))<=(h1/2)+(h2/2)
+ return abs(x2+w2/2-x1+w1/2)<=w1/2+w2/2 and abs(y2+h2/2-y1+h1/2)<=h1/2+h2/2
 end
